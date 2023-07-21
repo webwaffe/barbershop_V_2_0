b10f47c80152ee417aec04a8575c427b89ce6a2f
Andrei Naumchik <webwaffe@yandex.ru>
2023-07-21 13:03:54 +0300

    Add files via upload

---
+++ js/map.js
@@ -0,0 +1,24 @@
+
+let mapButton = document.querySelector(".contacts-button-map");
+
+let mapPopup = document.querySelector(".modal-map");
+let mapClose = mapPopup.querySelector(".modal-close");
+
+mapButton.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (mapPopup.classList.contains("modal-show")) {
+      mapPopup.classList.remove("modal-show");
+    }
+  }
+});
---
+++ js/popup.js
@@ -0,0 +1,60 @@
+
+
+let link = document.querySelector(".login-link");
+  
+let popup = document.querySelector(".modal-login");
+let close = popup.querySelector(".modal-close");
+
+let form = popup.querySelector("form");
+let login = popup.querySelector("[name=login]");
+let password = popup.querySelector("[name=password]");
+
+let isStorageSupport = true;
+let storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  
+  if (storage) {
+    login.value = storage;
+    password.focus();
+  } else {
+    login.focus();
+  }
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!login.value || !password.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", login.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
